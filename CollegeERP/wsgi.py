918763bb0ebd91398bd71a288b8115b17837e5ca
rajnandhinisymbsihs-droid <rajnandhini.symb.sihs@gmail.com>
2025-10-23 13:38:42 +0530

    test

--- CollegeERP/wsgi.py
+++ CollegeERP/wsgi.py
@@ -1,16 +1,19 @@
-"""
-WSGI config for CollegeERP project.
+# """
+# WSGI config for CollegeERP project.
 
-It exposes the WSGI callable as a module-level variable named ``application``.
+# It exposes the WSGI callable as a module-level variable named ``application``.
 
-For more information on this file, see
-https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
-"""
+# For more information on this file, see
+# https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+# """
 
-import os
+# import os
 
-from django.core.wsgi import get_wsgi_application
+# from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CollegeERP.settings')
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CollegeERP.settings')
 
-application = get_wsgi_application()
+# application = get_wsgi_application()
+
+
+COMMENTING OUT STUFF
